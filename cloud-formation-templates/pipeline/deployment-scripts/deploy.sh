709fd744ac3a7029465925e84c339af6a95ba973
Hotta, Abyaya <abyaya.hotta@ge.com>
2023-06-07 22:11:29 +0530

    modified the github link
    modified the github link

--- cloud-formation-templates/pipeline/deployment-scripts/deploy.sh
+++ cloud-formation-templates/pipeline/deployment-scripts/deploy.sh
@@ -2,10 +2,10 @@
 
 # Standard Fields
 ApplicationName="sc-data-orch" #sc-data-orch-fed-developer
-GitRepo="aws_rs_function_trigger_architecture" # Repo Name
-CICDTemplate="cicd-pipeline-deploy.yml" # resources CFT relative path from script location
+GitRepo="cicd-cloud-aws" # Repo Name
+CICDTemplate="cloud-formation-templates/pipeline/deployment-scripts/cicd-pipeline-deploy.yml" # resources CFT relative path from script location
 Region="us-east-1" # region
-GitHubURL="https://github.com/abyayaPrasad/aws_rs_function_trigger_architecture" # Github url
+GitHubURL="https://github.com/abyayaPrasad/cicd-cloud-aws.git" # Github url
 KMSKey="" # KMS key arn - create KMS key add ARN here
 BuildSpec="./cloud-formation-templates/pipeline/deployment-scripts/builspec-nonprod.yml" # Buildspec file name
 ResourceTemplate="./cloud-formation-templates/pipeline/resources/lambda-function.yml" # Absolute path from repo
