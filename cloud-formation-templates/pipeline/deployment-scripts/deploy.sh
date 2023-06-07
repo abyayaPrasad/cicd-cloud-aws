0dd313bf88c148fa5bc8e17b2bfed41a1cb4a346
Hotta, Abyaya <abyaya.hotta@ge.com>
2023-06-07 23:17:23 +0530

    Update on Non Prod Build Spec
    Update on Non Prod Build Spec

--- cloud-formation-templates/pipeline/deployment-scripts/deploy.sh
+++ cloud-formation-templates/pipeline/deployment-scripts/deploy.sh
@@ -7,11 +7,11 @@ CICDTemplate="cicd-pipeline-deploy.yml" # resources CFT relative path from scrip
 Region="us-east-1" # region
 GitHubURL="https://github.com/abyayaPrasad/cicd-cloud-aws.git" # Github url
 KMSKey="" # KMS key arn - create KMS key add ARN here
-BuildSpec="./cloud-formation-templates/pipeline/deployment-scripts/builspec-nonprod.yml" # Buildspec file name
+NonProdBuildSpec="./cloud-formation-templates/pipeline/deployment-scripts/builspec-nonprod.yml" # Buildspec file name
 ResourceTemplate="./cloud-formation-templates/pipeline/resources/lambda-function.yml" # Absolute path from repo
 PreProdCICDBucket="sc-data-orch-test-preprod" # Prod s3 bucket name
 ProdCICDBucket="sc-data-orch-test-preprod"
 stage_name="dev"
 
 
-aws cloudformation deploy --no-verify-ssl --stack-name "${ApplicationName}-codebuild-promotion-${GitRepo}" --template-file ${CICDTemplate} --region ${Region} --role-arn arn:aws:iam::767400217432:role/sc-cloudformation-deploy-role --parameter-overrides GitHubURL=${GitHubURL} KMSKey=${KMSKey} BuildSpec=${BuildSpec} Template=${ResourceTemplate} PreProdCICDBucket=${PreProdCICDBucket} ProdCICDBucket=${ProdCICDBucket} FileS3Prefix=${ApplicationName}-${GitRepo} Manifest=${Manifest}
+aws cloudformation deploy --no-verify-ssl --stack-name "${ApplicationName}-codebuild-promotion-${GitRepo}" --template-file ${CICDTemplate} --region ${Region} --role-arn arn:aws:iam::767400217432:role/sc-cloudformation-deploy-role --parameter-overrides GitHubURL=${GitHubURL} KMSKey=${KMSKey} BuildSpec=${NonProdBuildSpec} Template=${ResourceTemplate} PreProdCICDBucket=${PreProdCICDBucket} ProdCICDBucket=${ProdCICDBucket} FileS3Prefix=${ApplicationName}-${GitRepo} Manifest=${Manifest}
