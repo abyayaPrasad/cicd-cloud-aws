b9d418a195672c554460e2f1f15b8deb659616ba
Hotta, Abyaya <abyaya7861@gmail.com>
2023-06-09 16:54:17 +0530

    Env variables update
    Env variables update

--- cloud-formation-templates/pipeline/deployment-scripts/deploy.sh
+++ cloud-formation-templates/pipeline/deployment-scripts/deploy.sh
@@ -11,7 +11,8 @@ NonProdBuildSpec="cloud-formation-templates/pipeline/deployment-scripts/builspec
 ResourceTemplate="cloud-formation-templates/pipeline/resources/lambda-function.yml" # Absolute path from repo
 PreProdCICDBucket="sc-data-orch-test-preprod" # Prod s3 bucket name
 ProdCICDBucket="sc-data-orch-test-preprod"
-StageName="dev"
+StageNameNonProd="dev"
+Manifest=""
 
 
-aws cloudformation deploy --no-verify-ssl --stack-name "${ApplicationName}-codebuild-promotion-${GitRepo}" --template-file ${CICDTemplate} --region ${Region} --role-arn arn:aws:iam::767400217432:role/sc-cloudformation-deploy-role --parameter-overrides GitHubURL=${GitHubURL} KMSKey=${KMSKey} NonProdBuildSpec=${NonProdBuildSpec} Template=${ResourceTemplate} PreProdCICDBucket=${PreProdCICDBucket} ProdCICDBucket=${ProdCICDBucket} FileS3Prefix=${ApplicationName}-${GitRepo} ApplicationPrefix=${ApplicationName} StageName=${StageName} Manifest=${Manifest}
+aws cloudformation deploy --no-verify-ssl --stack-name "${ApplicationName}-codebuild-promotion-${GitRepo}" --template-file ${CICDTemplate} --region ${Region} --role-arn arn:aws:iam::767400217432:role/sc-cloudformation-deploy-role --parameter-overrides GitHubURL=${GitHubURL} KMSKey=${KMSKey} NonProdBuildSpec=${NonProdBuildSpec} Template=${ResourceTemplate} PreProdCICDBucket=${PreProdCICDBucket} ProdCICDBucket=${ProdCICDBucket} FileS3Prefix=${ApplicationName}-${GitRepo} ApplicationPrefix=${ApplicationName} StageName=${StageNameNonProd} Manifest=${Manifest}
