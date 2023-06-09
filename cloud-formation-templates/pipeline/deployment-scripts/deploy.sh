2f5c3b0219b5ec381890e7b944f72d3f5c38834c
Hotta, Abyaya <abyaya7861@gmail.com>
2023-06-09 16:57:24 +0530

    env variables update
    env variables update

--- cloud-formation-templates/pipeline/deployment-scripts/deploy.sh
+++ cloud-formation-templates/pipeline/deployment-scripts/deploy.sh
@@ -12,7 +12,7 @@ ResourceTemplate="cloud-formation-templates/pipeline/resources/lambda-function.y
 PreProdCICDBucket="sc-data-orch-test-preprod" # Prod s3 bucket name
 ProdCICDBucket="sc-data-orch-test-preprod"
 StageNameNonProd="dev"
-Manifest=""
+Manifest="requirements.txt"
 
 
 aws cloudformation deploy --no-verify-ssl --stack-name "${ApplicationName}-codebuild-promotion-${GitRepo}" --template-file ${CICDTemplate} --region ${Region} --role-arn arn:aws:iam::767400217432:role/sc-cloudformation-deploy-role --parameter-overrides GitHubURL=${GitHubURL} KMSKey=${KMSKey} NonProdBuildSpec=${NonProdBuildSpec} Template=${ResourceTemplate} PreProdCICDBucket=${PreProdCICDBucket} ProdCICDBucket=${ProdCICDBucket} FileS3Prefix=${ApplicationName}-${GitRepo} ApplicationPrefix=${ApplicationName} StageName=${StageNameNonProd} Manifest=${Manifest}
